be75cc17a6a511e94e80da3dfe20c0f9843076ad
N8449 <n8449@student.jamk.fi>
2020-03-01 16:45:55 +0200

    Deleting images

--- footer.php
+++ footer.php
@@ -1,31 +1,19 @@
-<!DOCTYPE HTML>
-<html lang="">
-<head>
-    <meta charset="UTF-8">
-    <title>Nome do Documento</title>
-	<link rel="stylesheet" type="text/css" href="discount.css">
-</head>
-    
-<body>
-  
-
-        
 <div class =" row footer">
-    <div class ="col-md-9">
+<div class ="col-md-9">
 &copy; <?php echo date ('Y'), "Student,Inc.All rights reserved";?>
 </div>
 
 
 <div class="col-md-3">
 <ul class="nav navbar-nav navbar-right icon">
-                            <li> <a class="footer- facebook " href="https://www.facebook.com/"><img src="facebook.png" height="25" width="25" alt="facebook icon"></a>
+                            <li> <a class="footer- facebook " href="https://www.facebook.com/"><img src="images/facebook.png" height="25" width="25" alt="facebook icon"></a>
                             </li>
 
                           
-                            <li> <a class="nav-link instagram" href="https://www.instagram.com/"><img src="instagram.png" height="25" width="25" alt="instagram icon"></a>
+                            <li> <a class="nav-link instagram" href="https://www.instagram.com/"><img src="images/instagram.png" height="25" width="25" alt="instagram icon"></a>
                             </li>
 
-                            <li> <a class="nav-link gmail" href="mailto:neuvo.fms.fi@discount.com"><img src="email.png" height="25" width="25" alt="Email icon"></a>
+                            <li> <a class="nav-link gmail" href="mailto:neuvo.fms.fi@discount.com"><img src="images/email.png" height="25" width="25" alt="Email icon"></a>
                             </li>
 
 
@@ -34,6 +22,6 @@
 </div> <!--Footer-->
 
 </div> <!--Container--> 
-
+</div> <!--header-->
 </body>
 </html>
