e1c8e1c89e47135c56d9c687051f6959f0519783
N8449 <n8449@student.jamk.fi>
2020-03-01 13:33:34 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,39 +1,45 @@
-<?php include('server.php') ?>
-<!DOCTYPE html>
+<?php include('server.php'); ?>
+<!DOCTYPE HTML>
 <html>
 <head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+    
+    <title>registered user</title>
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <div class="header">
-  	<h2>Register</h2>
-  </div>
-	
-  <form method="post" action="register.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  	  <label>Username</label>
-  	  <input type="text" name="username" value="<?php echo $username; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Email</label>
-  	  <input type="email" name="email" value="<?php echo $email; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Password</label>
-  	  <input type="password" name="password_1">
-  	</div>
-  	<div class="input-group">
-  	  <label>Confirm password</label>
-  	  <input type="password" name="password_2">
-  	</div>
-  	<div class="input-group">
-  	  <button type="submit" class="btn" name="reg_user">Register</button>
-  	</div>
-  	<p>
+<div class="header">
+<h2>Register</h2>
+</div>
+<form method="post" action="register.php" >
+
+<!-- Validation Error -->
+<?php include('errors.php'); ?>
+<div class="input-group">
+<label>Username</label>
+<input type="text" name="username" value="<?php echo $username; ?>"> </div>
+
+<div class="input-group">
+<label>Email</label>
+<input type="text" name="email" value=" <?php echo $email; ?>">
+</div>
+
+<div class="input-group">
+<label>Password</label>
+<input type="password" name="password_1">
+</div>
+
+<div class="input-group">
+<label>Confirm Password</label>
+<input type="password" name="password_2">
+</div>
+
+<div class="input-group">
+<button type="submit" name="reg_user" class="btn">Register</button>
+</div>
+<p>
   		Already a member? <a href="login.php">Sign in</a>
   	</p>
-  </form>
+</form>
+    
 </body>
 </html>
