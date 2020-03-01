32972f9c1153e49e1d108e0ca72967658d7c3368
N8449 <n8449@student.jamk.fi>
2020-03-01 16:30:14 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,8 +11,30 @@
 
         
 <div class =" row footer">
-&copy; <?php echo date ('Y'); ?>
+    <div class ="col-md-9">
+&copy; <?php echo date ('Y'), "Student,Inc.All rights reserved";?>
+</div>
 
+<ul class="nav navbar-nav navbar-right icon">
+                            <li> <a class="footer- facebook " href="https://www.facebook.com/"><img src="iconmonstr-facebook-1-72.png" height="25" width="25" alt="facebook icon"></a>
+                            </li>
+
+                            <li> <a class="nav-link call" href="tel:358105407000"><img src="iconmonstr-phone-1-72.png" height="25" width="25" alt="call icon"></a>
+                            </li>
+
+                            <li> <a class="nav-link instagram" href="https://www.instagram.com/sodexogroup/"><img src="iconmonstr-instagram-11-72.png" height="25" width="25" alt="instagram icon icon"></a>
+                            </li>
+
+                            <li> <a class="nav-link gmail" href="mailto:neuvo.fms.fi@sodexo.com"><img src="iconmonstr-email-2-72.png" height="25" width="25" alt="Email icon"></a>
+                            </li>
+
+
+
+                        </ul>
+<div class="col-md-3">
+
+
+</div>
 </div> <!--Footer-->
 
 </div> <!--Container--> 
