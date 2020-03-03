4a0411098398d672b4c2f391618d772b1ca162a6
MattEveritt <60779706+MattEveritt@users.noreply.github.com>
2020-03-03 10:51:51 +0200

    Create search_form.php

---
+++ search_form.php
@@ -0,0 +1,9 @@
+
+<div class='col-md-9 data'>
+    <h2>Search discounts</h2>
+    
+    <form action="search.php" method="GET" name="search_form">
+        <input class="text-box" type="text" name="query" /><br>
+        <input class="btn" type="submit" value="Search" style="float: right" />
+    </form>
+</div>
