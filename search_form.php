e81355cf721a3ae3fe18d3290eeb4ae202506241
N8449 <n8449@student.jamk.fi>
2020-03-04 18:10:33 +0200

    Update search_form.php

--- search_form.php
+++ search_form.php
@@ -1,5 +1,8 @@
 
 <?php
+if($_GET['q'] == 'search...'){
+    header('location: search_form.php');
+}
 if($_GET['q'] !== ''){
     $db = mysql_connect('localhost', 'matt', 'Scooby1990!', 'discountdb');
 
@@ -63,6 +66,15 @@ searchBar.placeholder = ''
       margin:0;
       padding:0;
   }
+
+  a{
+text-decoration: underline;
+  }
+
+  a:hover{
+color: #000000;
+text-decoration: none;
+  }
         </style>
 </head>
     
@@ -72,9 +84,19 @@ searchBar.placeholder = ''
     <input type="submit" id="searchBtn"  value="search" />
 </form>
     <?php
+
+    if(!isset($q)){
+echo '';
+    }else{
+
+    
+
+
     $query = mysql_query("SELECT * FROM discounts WHERE discount_percent LIKE '%$q%' OR discount_description LIKE '%$q%' ");
     $num_rows = mysql_num_rows($query);
-
+?>
+<p><strong><?php echo $num_rows; ?> </strong> results for '<?php echo $q; ?>' </p>
+<?php
  while($row = mysql_fetch_array($query)){
    $id = $row['id'];
    $discount_description = $row['discount_description'];
@@ -86,11 +108,14 @@ searchBar.placeholder = ''
    $phone = $row['phone'];
    $email = $row['email'];
 
-   echo   ' <h3>' . $discount_description . ' </h3>' .  $normal_price . ' ' .  $student_price . ' ' . $discount_percent . ' ' . $days_of_week . ' ' .  $address . ' ' .  $phone . ' ' . $email . '<br/>';
+   echo   ' <h3><a href="' . $id . '">' . $discount_description . ' </h3>' .  $normal_price . ' ' .  $student_price . ' ' . $discount_percent . ' ' . $days_of_week . ' ' .  $address . ' ' .  $phone . ' ' . $email . '<br/>';
  }
+}
     ?>
 </body>
 </html>
 <?php
+} else {
+    header('location: search_form.php');
 }
 ?>
