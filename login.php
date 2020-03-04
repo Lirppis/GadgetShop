75828bed676b11d1ab5809eb124fe2d7320c525a
N8449 <n8449@student.jamk.fi>
2020-03-04 09:27:17 +0200

    Update login.php

--- login.php
+++ login.php
@@ -32,7 +32,7 @@ English  Finnish
   		<button type="submit" class="btn" name="login_user">Login</button>
   	</div>
   	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
+  		
   	</p>
   </form>
 </div>
