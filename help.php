82d6df002c2fe3c08cde8e4852ca062425087764
Lirppis <Lirppis@users.noreply.github.com>
2020-03-04 19:54:16 +0200

    Update help.php

--- help.php
+++ help.php
@@ -1,5 +1,25 @@
 <?php include 'header.php'?>
 
+<html>
+<body>
+    <div class="faq">
+    <h1> Frequently asked questions </h1>
+
+    <li> Question 1 </li>
+    <p> Answer1 </p>
+    <li> Question 1 </li>
+    <p> Answer1 </p>
+    <li> Question 1 </li>
+    <p> Answer1 </p>
+    <li> Question 1 </li>
+    <p> Answer1 </p>
+    <li> Question 1 </li>
+    <p> Answer1 </p>
+
+
+    </div>
+</body>
+<html>
 
 
 
