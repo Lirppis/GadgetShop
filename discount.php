e8b562ce46888446d7e9889615092640ecd19da9
Lirppis <Lirppis@users.noreply.github.com>
2020-03-04 23:04:50 +0200

    Update discount.php

--- discount.php
+++ discount.php
@@ -1,3 +1,22 @@
 <?php include 'header.php'?>
-<?php include 'search_form.php'?>
+<?php include 'search_form.html'?>
+<html>
+<body>
+
+    <div class="footer-fix" >
+        <p>l</p>
+        <p>1</p>
+        <p>l</p>
+        <p>l</p>
+        <p>l</p>
+        <p>l</p>
+        <p>l</p>
+    </div>
+</body>
+</html>
+
+
+
+
+
 <?php include 'footer.php'?>
