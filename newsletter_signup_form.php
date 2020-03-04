86c3efd59fb59d6c42d74ca7e02ad932a793c453
N8449 <n8449@student.jamk.fi>
2020-03-04 12:34:27 +0200

    update

--- newsletter_signup_form.php
+++ newsletter_signup_form.php
@@ -3,7 +3,7 @@
 <div class='col-md-9 data'>
     <h2>Signup to our <br> newsletter</h2>
 <form name= "newsletter_signup_form" method="post" action="newsletter_signup.php">
-    Name: <input class="text-box" type="text" name="name"><br>
+    Name: <input class="text-box" type="text" name="name" ><br>
     Email: <input class="text-box" type="email" name="email"><br>
 <input class="btn" type="submit" value="Submit" style="float: right">
 </form>
