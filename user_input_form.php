85db3d0e9e248c26b23da7871d92ab765744459a
N8449 <n8449@student.jamk.fi>
2020-03-04 13:56:32 +0200

    update

--- user_input_form.php
+++ user_input_form.php
@@ -1,11 +1,33 @@
 <?php include 'header.php' ?>
+<!DOCTYPE HTML>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <title>Nome do Documento</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+    
+<body>
+
+<script>
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+</script>
+    
+</body>
+</html>
 
 <div class='col-md-9 data'>
     <h2>Add a discount</h2>
-<form name= "user_input_form" method="post" action="create_verification_request.php">
-    Business Name: <input class="text-box" type="text" name="business_name"><br>
-    Normal Price: <input class="text-box" type="number" name="user_normal_price"><br>
-    Student Price: <input class="text-box" type="number" name="user_student_price"><br>
+<form name= "user_input_form" onsubmit="return validateform()" method="post" action="create_verification_request.php">
+    Business Name: <input class="text-box" type="text" name="business_name"required><br>
+    Normal Price: <input class="text-box" type="number" name="user_normal_price"required><br>
+    Student Price: <input class="text-box" type="number" name="user_student_price"required><br>
     City/Town: <input class="text-box" type="text" name="user_city"><br>
 <input class="btn" type="submit" value="Submit" style="float: right">
 </form>
