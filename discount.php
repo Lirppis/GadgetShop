46e0c7410d238bf873279cc4b5ea23c1887a2040
MattEveritt <60779706+MattEveritt@users.noreply.github.com>
2020-03-04 23:52:26 +0200

    q

--- discount.php
+++ discount.php
@@ -1,24 +1,3 @@
 <?php include 'header.php'?>
-<?php include 'search_form.html'?>
-<html>
-<body>
-
-    <div class="footer-fix" >
-        <p>l</p>
-        <p>1</p>
-        <p>l</p>
-        <p>l</p>
-        <p>l</p>
-        <p>l</p>
-        <p>l</p>
-    </div>
-</body>
-</html>
-
-
-
-
-
-<?php include 'footer.php'?>
 <?php include 'index_searchbar.php'?>
 <?php include 'footer.php'?>
