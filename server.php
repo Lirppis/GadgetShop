28403debe2f0706cc8c60ee15c40eb16b5960829
MattEveritt <60779706+MattEveritt@users.noreply.github.com>
2020-03-04 12:57:28 +0200

    h

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'registration');
+$db = mysqli_connect('localhost', 'matt', 'Scooby1990!', 'discountdb');
 
 
 //login user
